3a539ce3c22c9a0c8708363bd044b47ef9f2caad
Fabricio Catae <fcatae@hotmail.com>
2019-12-03 22:15:58 -0300

    initial commit

---
+++ Oraculo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using StackExchange.Redis;
+
+namespace ConsoleOraculo
+{
+    class Oraculo
+    {
+        const string canal = "Perguntas";
+        private readonly ISubscriber _pub;
+        private readonly IDatabase _db;
+
+        public Oraculo(string connectionString)
+        {
+            var redis = ConnectionMultiplexer.Connect(connectionString);
+            this._pub = redis.GetSubscriber();
+            this._db = redis.GetDatabase();
+        }
+
+        public string Pergunta(string id, string pergunta)
+        {
+            string mensagem = $"{id}: {pergunta}";
+            _pub.Publish(canal, mensagem);
+
+            return mensagem;
+        }
+
+        public Resposta[] ObterRespostas(string id)
+        {
+            return _db.HashGetAll(id)
+                .Select(v => new Resposta
+                {
+                    Equipe = v.Name,
+                    Texto = v.Value
+                })
+                .ToArray();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ConsoleOraculo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hostname = (args.Length > 0) ? args[0] : "localhost";
+
+            Console.WriteLine("Conectando em " + hostname);
+
+            var oraculo = new Oraculo(hostname);
+
+            for(int i=1; ;i++)
+            {
+                string id = $"P{i}";
+                string pergunta = "Qual a capital do Brasil?";
+                string mensagem = oraculo.Pergunta(id, pergunta);
+
+                Console.WriteLine(mensagem);
+                Task.Delay(5000).Wait();
+
+                var respostas = oraculo.ObterRespostas(id);
+                foreach(var resp in respostas)
+                {
+                    Console.WriteLine($"{resp.Equipe}: {resp.Texto}");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
